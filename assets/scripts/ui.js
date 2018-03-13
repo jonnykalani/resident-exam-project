a200911a0974663cc12843cfacfa34da1274d4b5
jonnykalani <jonathankhicks@gmail.com>
2018-03-13 14:51:57 -0400

    Added some ui functionality
    made it so the sign in etc gives feedback and hides

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,18 +4,19 @@ const store = require('./store')
 
 const signUpSuccess = function (data) {
   console.log('signed up, data is ', data)
-  $('#sign-up-message').text('Signed up!')
+  $('#status-message').text('Signed up!')
   $('#sign-up').text('Signed up!')
 }
 
 const error = function () {
   console.log('something went wrong')
-  $('#sign-up-message').text('error on sign up')
+  $('#status-message').text('error on sign up')
 }
 
 const signInSuccess = function (data) {
-  $('#top-message').text('Signed In Successfully')
-  $('#sign-in-message').css('background-color', 'green')
+  $('#status-message').text('Signed In Successfully')
+  $('#status-message').css('background-color', 'green')
+  $('#container-sign').hide()
   console.log(data)
   store.user = data.user
   console.log('Signed in')
@@ -23,33 +24,32 @@ const signInSuccess = function (data) {
 
 const signInFailure = function (error) {
   console.log(error)
-  $('#sign-in-message').text('Error on logging in')
-  $('#sign-in-message').css('font-size', '48px')
-  $('#sign-up-message').text('')
+  $('#status-message').text('Error on logging in')
+  $('#status-message').css('font-size', '48px')
+  $('#status-message').text('')
 }
 
 const changePasswordSuccess = function (data) {
-  $('#top-message').text('')
-  $('#change-message').text('change success!')
+  $('#status-message').text('change success!')
   console.log('change success!')
 }
 
 const changePasswordFailure = function (error) {
-  $('#change-message').text('Error on change password')
-  $('#change-message').css('font-size', '48px')
-  $('#bottom-message').text('change error!')
+  $('#status-message').text('Error on change password')
+  $('#status-message').css('font-size', '48px')
+  $('#status-message').text('change error!')
   console.log(error)
 }
 
 const signOutSuccess = function (data) {
-  $('#top-message').text('')
-  $('#sign-out-message').text('Successfully signed out!')
+  $('#status-message').text('')
+  $('#status-message').text('Successfully signed out!')
 }
 
 const signOutFailure = function (data) {
-  $('#top-message').text('')
-  $('#sign-out-message').text('error on sign out')
-  $('#sign-out-message').css('font-size', '48px')
+  $('#status-message').text('')
+  $('#status-message').text('error on sign out')
+  $('#status-message').css('font-size', '48px')
 }
 
 module.exports = {

2ec532b913f237b8044d5bee956bc8b5ca320e33
jonnykalani <jonathankhicks@gmail.com>
2018-03-13 11:43:39 -0400

    Fixed a bug requiring events.js
    bug in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-const appEvents = require('/events.js')
+const appEvents = require('./events.js')
 
 $(() => {
   setAPIOrigin(location, config)
